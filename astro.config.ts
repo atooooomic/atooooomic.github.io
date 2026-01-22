7fbd4a79e3023aeafd8f7971abfab553b018654a
atooooomic <rieul.tech@gmail.com>
2026-01-22 10:06:01 +0900

    Add Google Analytics (GA4) integration (#14)

--- astro.config.ts
+++ astro.config.ts
@@ -60,6 +60,11 @@ export default defineConfig({
         context: "client",
         optional: true,
       }),
+      PUBLIC_GOOGLE_ANALYTICS_ID: envField.string({
+        access: "public",
+        context: "client",
+        optional: true,
+      }),
     },
   },
   experimental: {
